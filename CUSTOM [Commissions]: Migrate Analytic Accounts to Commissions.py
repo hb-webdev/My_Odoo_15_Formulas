f0a70713428ba96d3b2ce3aff978f924d636e01a
hb-webdev <85006632+hb-webdev@users.noreply.github.com>
2023-07-03 12:14:17 -0600

    Update CUSTOM [Commissions]: Migrate Analytic Accounts to Commissions.py

--- CUSTOM [Commissions]: Migrate Analytic Accounts to Commissions.py
+++ CUSTOM [Commissions]: Migrate Analytic Accounts to Commissions.py
@@ -1,3 +1,5 @@
+# SERVER ACTION
+
 # Available variables:
 #  - env: Odoo Environment on which the action is triggered
 #  - model: Odoo Model of the record on which the action is triggered; is a void recordset
